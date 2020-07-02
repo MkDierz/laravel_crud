114c37ac22ac3528c5988faeb2700ded0985ea93
MkDierz <msyibbran24@gmail.com>
2020-07-02 21:40:19 +0700

    update template

--- resources/views/admin/content.blade.php
+++ resources/views/admin/content.blade.php
@@ -1,3 +1,6 @@
+@extends('admin.master')
+
+@section('content')
 <!-- Content Header (Page header) -->
 <section class="content-header">
     <div class="container-fluid">
@@ -20,28 +23,13 @@
 
     <!-- Default box -->
     <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Title</h3>
-
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip"
-                        title="Collapse">
-                    <i class="fas fa-minus"></i></button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip"
-                        title="Remove">
-                    <i class="fas fa-times"></i></button>
-            </div>
-        </div>
         <div class="card-body">
             Start creating your amazing application!
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-            Footer
-        </div>
-        <!-- /.card-footer-->
     </div>
     <!-- /.card -->
 
 </section>
 <!-- /.content -->
+@endsection
