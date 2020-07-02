fe414250303f9695fc19d08b85048d4d871eb684
MkDierz <msyibbran24@gmail.com>
2020-07-03 04:01:31 +0700

    add controler

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\answer;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    //
+    public function index($id)
+    {
+        return view('answer');
+    }
+
+    public function store($id,Request $request)
+    {
+//        dd($id);
+        $data = $request->all();
+//        dd($data);
+        unset($data["_token"]);
+        unset($data["files"]);
+        $data['date_create'] = Carbon::now()->toDateString();
+        $data['question_id'] = $id;
+//        dd($data);
+        $item = answer::insert($data);
+        if ($item) {
+            return redirect('/pertanyaan');
+        }
+    }
 }
--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\question;
+use Carbon\Carbon;
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = question::get_all();
+        return view('index', compact('items'));
+    }
+
+    public function create()
+    {
+        return view('question_form');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        unset($data["_token"]);
+        unset($data["files"]);
+        $data['date_create'] = Carbon::now()->toDateString();
+        $item = question::insert($data);
+        if ($item) {
+            return redirect('/pertanyaan');
+        }
+    }
 }
