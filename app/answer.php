c5a88a0359b5e062f81eb179cec9c4bdf7952480
MkDierz <msyibbran24@gmail.com>
2020-07-03 04:02:07 +0700

    add model

--- app/answer.php
+++ app/answer.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use http\Env\Request;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class answer extends Model
 {
-    //
+    public static function get_all()
+    {
+        return DB::table('answers')->get();
+    }
+    public static function get_user_id($id)
+    {
+        return DB::table('answers')->where('user_id',$id)->get();
+    }
+    public static function insert($data)
+    {
+        return DB::table('answers')->insert($data);
+    }
+
 }
--- app/question.php
+++ app/question.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class question extends Model
 {
     //
+    public static function get_all()
+    {
+        return DB::table('questions')->get();
+    }
+
+    public static function insert($data)
+    {
+        return DB::table('questions')->insert($data);
+    }
 }
