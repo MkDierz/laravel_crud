cca5f45cd226513e7066f7d8a3697e39b4157231
MkDierz <msyibbran24@gmail.com>
2020-07-02 21:41:05 +0700

    update migration

--- database/migrations/2020_07_02_134253_create_questions_table.php
+++ database/migrations/2020_07_02_134253_create_questions_table.php
@@ -15,6 +15,12 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->date('date_edit');
+            $table->date('date_update');
+            $table->integer('like');
+            $table->integer('dislike');
             $table->timestamps();
         });
     }
