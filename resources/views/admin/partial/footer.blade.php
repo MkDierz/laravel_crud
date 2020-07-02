d75a9b01aa6c719bad42328cf79a49f0c0bc2b81
MkDierz <msyibbran24@gmail.com>
2020-07-02 20:41:23 +0700

    add adminlte

---
+++ resources/views/admin/partial/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <i>Wir sind nicht frei in dem, was wir tun, weil wir nicht frei sind in dem, was wir wollen.</i>
+    </div>
+    <strong>Copyright &copy; 2040 <a href="https://github.com/MkDierz">MkDierz</a></strong>
+
+</footer>
---
+++ resources/views/admin/partial/main_sidebar.blade.php
@@ -0,0 +1,39 @@
+<aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/" class="brand-link">
+        <img src="{{asset('/adminlte/dist/img/logo-horizontal.png')}}"
+             alt="AdminLTE Logo"
+{{--             class="brand-image img-circle elevation-3"--}}
+             class="brand-image"
+             style="opacity: .8;width: 35px;height: 40px;">
+        <span class="brand-text font-weight-bold">SanberCode</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+
+                <li class="nav-header">Pages</li>
+                <li class="nav-item">
+                    <a href="/" class="nav-link">
+                        <i class="fas fa-home nav-icon"></i>
+                        <p>Home</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/data-tables" class="nav-link">
+                        <i class="fas fa-table nav-icon"></i>
+                        <p>Data Tables</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
