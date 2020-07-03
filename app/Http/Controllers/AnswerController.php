122c8101cbd299762847c4fa0ed5335b71d15ef4
MkDierz <msyibbran24@gmail.com>
2020-07-04 01:53:45 +0700

    update controler

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -10,22 +10,18 @@ class AnswerController extends Controller
 {
     public function index($id)
     {
-        return view('answer');
+        return view('jawaban_add',['id'=>$id]);
     }
 
     public function store($id,Request $request)
     {
-//        dd($id);
         $data = $request->all();
-//        dd($data);
         unset($data["_token"]);
         unset($data["files"]);
-        $data['date_create'] = Carbon::now()->toDateString();
         $data['question_id'] = $id;
-//        dd($data);
         $item = answer::insert($data);
         if ($item) {
-            return redirect('/pertanyaan');
+            return redirect('/pertanyaan/'.$id);
         }
     }
 }
--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,30 +4,76 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\question;
+use App\answer;
 use Carbon\Carbon;
+use Throwable;
 
 class QuestionController extends Controller
 {
     public function index()
     {
         $items = question::get_all();
-        return view('index', compact('items'));
+        return view('pertanyaan_index', compact('items'));
     }
 
     public function create()
     {
-        return view('question_form');
+        return view('pertanyaan_add');
     }
 
-    public function store(Request $request)
+    public function insert(Request $request)
     {
         $data = $request->all();
         unset($data["_token"]);
-        unset($data["files"]);
-        $data['date_create'] = Carbon::now()->toDateString();
         $item = question::insert($data);
         if ($item) {
             return redirect('/pertanyaan');
         }
     }
+
+    public function view($id)
+    {
+        $answer = answer::get_source($id);
+        try {
+            // Validate the value...
+            $answer[0];
+            $data_out['answer'] = $answer;
+
+        } catch (Throwable $e) {
+//            report($e);
+//            dd($answer);
+//            return false;
+            $data_out['answer'] = false;
+        }
+        $data = question::get_id($id);
+        $data_out['question'] = $data[0];
+        return view('pertanyaan_detail',compact('data_out'));
+    }
+
+    public function edit($id)
+    {
+        $data = question::get_id($id)[0];
+        return view('pertanyaan_edit',compact('data'));
+    }
+
+    public function update($id,Request $request)
+    {
+        unset($request["_token"]);
+//        dd($request->all());
+//        question::whereId($id)->update($request->all());
+        $request['id'] = $id;
+        question::update_tbl($request->all());
+        if ($request) {
+            return redirect('/pertanyaan/'.$id);
+        }
+
+    }
+
+    public function delete($id)
+    {
+        question::delete_tbl($id);
+        return redirect('/pertanyaan');
+    }
+
+//    public function
 }
